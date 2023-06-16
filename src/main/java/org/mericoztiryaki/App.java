ade3e4a392f6e8226158c33780843d1edde9259d
Meriç Öztiryaki <oztiryakimeric@gmail.com>
2023-06-16 14:32:35 +0300

    Initial code upload

---
+++ src/main/java/org/mericoztiryaki/App.java
@@ -0,0 +1,11 @@
+package org.mericoztiryaki;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println( "Hello World!" );
+    }
+}
