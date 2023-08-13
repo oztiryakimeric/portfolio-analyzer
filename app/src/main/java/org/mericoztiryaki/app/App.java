352ffddc5ea4db5ef55aa7ddd0b22723e3c915ba
Meriç Öztiryaki <oztiryakimeric@gmail.com>
2023-08-13 19:18:28 +0300

    CLI options added.

--- app/src/main/java/org/mericoztiryaki/app/App.java
+++ app/src/main/java/org/mericoztiryaki/app/App.java
@@ -1,5 +1,6 @@
 package org.mericoztiryaki.app;
 
+import org.apache.commons.cli.*;
 import org.mericoztiryaki.app.adapter.PriceApiAdapter;
 import org.mericoztiryaki.app.reader.CsvReader;
 import org.mericoztiryaki.app.reader.PortfolioReader;
@@ -18,33 +19,74 @@ import org.mericoztiryaki.domain.service.impl.TransactionService;
 import java.io.IOException;
 import java.nio.file.Paths;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Set;
 
 public class App {
 
-    public static void main(String[] args) throws IOException, PriceApiException {
-        PriceSource priceSource = new PriceApiAdapter();
-        PriceService priceService = new PriceService(priceSource);
-        TransactionService transactionService = new TransactionService(priceService);
-        ReportService reportService = new ReportService(priceService, transactionService);
+    private static PriceSource priceSource;
+    private static PriceService priceService;
+    private static TransactionService transactionService;
+    private static ReportService reportService;
+    private static ReportManager reportManager;
 
-        ReportManager reportManager = new ReportManager(reportService);
+    public static void main(String[] args) throws IOException, ParseException {
+        initializeBeans();
 
-        PortfolioReader csvReader = new CsvReader("/Users/meric/dev/portfolio-vis/.dev-space/dev-portfolio-3.csv");
-
-        ReportRequest reportRequest = ReportRequest.builder()
-                .transactions(csvReader.read())
-                .reportDate(LocalDate.now())
-                .periods(Set.of(Period.D1, Period.W1, Period.M1, Period.ALL))
-                .pnlHistoryUnits(Set.of(PnlHistoryUnit.DAY, PnlHistoryUnit.WEEK, PnlHistoryUnit.MONTH, PnlHistoryUnit.YEAR))
-                .currencies(Set.of(Currency.TRY, Currency.USD))
-                .outputType(ReportOutputType.EXCEL)
-                .outputFileLocation(Paths.get("").toAbsolutePath().toString() + "/out.xlsx")
-                .build();
-
-        reportManager.generateReport(reportRequest);
+        createReport(readReportRequest(args));
 
         System.out.println("EDOM");
+    }
+
+    private static void initializeBeans() {
+        priceSource = new PriceApiAdapter();
+        priceService = new PriceService(priceSource);
+        transactionService = new TransactionService(priceService);
+        reportService = new ReportService(priceService, transactionService);
+        reportManager = new ReportManager(reportService);
+    }
+
+    private static ReportRequest readReportRequest(String[] args) throws ParseException {
+        CommandLineParser parser = new DefaultParser();
+
+        Options options = new Options();
+        options.addOption(Option.builder()
+                .option("d")
+                .longOpt("date")
+                .hasArg()
+                .desc("Date the report will be generated")
+                .build());
+
+        options.addOption(Option.builder()
+                .option("i")
+                .longOpt("input-file")
+                .hasArg()
+                .required()
+                .desc("Csv file path which transactions defined in")
+                .build());
+
+        ReportRequest reportRequest = ReportRequest.getDefaultReportRequest();
+        CommandLine line = parser.parse(options, args);
+
+        if (line.hasOption("date")) {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("DD-MM-YYYY");
+            LocalDate date = LocalDate.parse(line.getOptionValue("date"), formatter);
+
+            reportRequest.setReportDate(date);
+        }
+
+        if (line.hasOption("input-file")) {
+            reportRequest.setInputFileLocation(line.getOptionValue("input-file"));
+        }
+
+        return reportRequest;
+    }
+
+    private static void createReport(ReportRequest reportRequest) throws IOException {
+        PortfolioReader csvReader = new CsvReader(reportRequest.getInputFileLocation());
+        reportRequest.setTransactions(csvReader.read());
+
+        reportManager.generateReport(reportRequest);
     }
 
 }
--- domain/src/main/java/org/mericoztiryaki/domain/model/ReportRequest.java
+++ domain/src/main/java/org/mericoztiryaki/domain/model/ReportRequest.java
@@ -6,6 +6,7 @@ import lombok.Setter;
 import org.mericoztiryaki.domain.model.constant.*;
 import org.mericoztiryaki.domain.model.transaction.TransactionDefinition;
 
+import java.nio.file.Paths;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Set;
@@ -14,6 +15,8 @@ import java.util.Set;
 @Getter
 @Builder
 public class ReportRequest {
+
+    private String inputFileLocation;
 
     private List<TransactionDefinition> transactions;
 
@@ -31,4 +34,15 @@ public class ReportRequest {
 
     private String outputFileLocation;
 
+    public static ReportRequest getDefaultReportRequest() {
+        // Only inputFileLocation field required
+        return ReportRequest.builder()
+                .reportDate(LocalDate.now())
+                .currencies(Set.of(Currency.USD, Currency.TRY))
+                .pnlHistoryUnits(Set.of(PnlHistoryUnit.DAY, PnlHistoryUnit.WEEK, PnlHistoryUnit.MONTH, PnlHistoryUnit.YEAR))
+                .periods(Set.of(Period.D1, Period.W1, Period.M1, Period.ALL))
+                .outputType(ReportOutputType.EXCEL)
+                .outputFileLocation(Paths.get("").toAbsolutePath().toString() + "/report.xlsx")
+                .build();
+    }
 }
