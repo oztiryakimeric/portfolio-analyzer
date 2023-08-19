8459d3597ba17d171fdee98f274cf6384d593d1a
Meriç <oztiryakimeric@gmail.com>
2023-08-19 09:30:14 +0300

    Price cache file name changed.

--- domain/src/main/java/org/mericoztiryaki/domain/util/Environment.java
+++ domain/src/main/java/org/mericoztiryaki/domain/util/Environment.java
@@ -4,7 +4,7 @@ public class Environment {
 
     public static final String PRICE_API_HOST = readEnvVariable("PRICE_API_HOST", "http://127.0.0.1:8000");
 
-    public static final String PRICE_CACHE_PATH = readEnvVariable("PRICE_CACHE_PATH", System.getProperty("java.io.tmpdir") + "priceCache.bin");
+    public static final String PRICE_CACHE_PATH = readEnvVariable("PRICE_CACHE_PATH", System.getProperty("java.io.tmpdir") + "pvis-price-cache.bin");
 
     private static String readEnvVariable(String name, String defaultValue) {
         String val = System.getenv(name);
